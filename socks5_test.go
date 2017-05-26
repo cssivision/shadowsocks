cbe7733327f348a089097d8f1e782e65bb04da99
cssivision <cssivision@gmail.com>
2017-05-26 21:58:58 +0800

    test: socket negotiate

--- socks5_test.go
+++ socks5_test.go
@@ -1,1 +1,40 @@
 package shadowsocks
+
+import (
+	"net"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"golang.org/x/net/proxy"
+)
+
+func TestSocks5Negotiate(t *testing.T) {
+
+	for _, url := range []string{"looli.xyz:80", "127.0.0.1:9898", "::1:9999"} {
+		addr := ":3002"
+
+		go func() {
+			forward := proxy.FromEnvironment()
+			dailer, err := proxy.SOCKS5("tcp", addr, nil, forward)
+			assert.Nil(t, err)
+
+			conn, err := dailer.Dial("tcp", url)
+			assert.Nil(t, err)
+			defer conn.Close()
+		}()
+
+		lis, err := net.Listen("tcp", addr)
+		assert.Nil(t, err)
+
+		for {
+			conn, err := lis.Accept()
+			assert.Nil(t, err)
+
+			rawaddr, host, err := Socks5Negotiate(conn)
+			assert.Nil(t, err)
+			assert.Equal(t, "looli.xyz:80", host)
+			assert.Equal(t, []byte{0x3, 0x9, 0x6c, 0x6f, 0x6f, 0x6c, 0x69, 0x2e, 0x78, 0x79, 0x7a, 0x0, 0x50}, rawaddr)
+			return
+		}
+	}
+}
