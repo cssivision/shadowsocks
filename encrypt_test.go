6a98210ffc3777565328954799c3a6d5a43e853b
cssivision <cssivision@gmail.com>
2017-08-04 16:24:40 +0800

    add test for cipher method

--- encrypt_test.go
+++ encrypt_test.go
@@ -13,8 +13,30 @@ func TestNewCipher(t *testing.T) {
 		assert.NotNil(t, err)
 	})
 
-	t.Run("normal new cipher", func(t *testing.T) {
+	t.Run("cipher aes-128-cfb", func(t *testing.T) {
 		cipher, err := NewCipher("aes-128-cfb", "password")
+		assert.Nil(t, err)
+		assert.NotNil(t, cipher)
+		assert.Equal(t, 16, cipher.info.keyLen)
+		assert.Equal(t, 16, cipher.info.ivLen)
+		assert.Equal(t, generateKey("password", 16), cipher.key)
+		assert.Nil(t, cipher.enc)
+		assert.Nil(t, cipher.dec)
+	})
+
+	t.Run("cipher aes-256-cfb", func(t *testing.T) {
+		cipher, err := NewCipher("aes-256-cfb", "password")
+		assert.Nil(t, err)
+		assert.NotNil(t, cipher)
+		assert.Equal(t, 32, cipher.info.keyLen)
+		assert.Equal(t, 16, cipher.info.ivLen)
+		assert.Equal(t, generateKey("password", 32), cipher.key)
+		assert.Nil(t, cipher.enc)
+		assert.Nil(t, cipher.dec)
+	})
+
+	t.Run("cipher rc4-md5", func(t *testing.T) {
+		cipher, err := NewCipher("rc4-md5", "password")
 		assert.Nil(t, err)
 		assert.NotNil(t, cipher)
 		assert.Equal(t, 16, cipher.info.keyLen)
